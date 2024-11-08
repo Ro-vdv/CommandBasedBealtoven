2c0ffdbdbfd136ead6c207a2d20288bf31b9be74
Ro-vdv <ronanvdvlag@hotmail.com>
2024-11-08 14:47:46 -0500

    untested shooter

--- src/main/java/frc/robot/commands/ShootingCmd.java
+++ src/main/java/frc/robot/commands/ShootingCmd.java
@@ -13,43 +13,44 @@ import frc.robot.subsystems.Shooter.ShooterSetpoint;
 public class ShootingCmd extends Command {
 
 private final Shooter shooterSubsystem;
- private final Kicker kickerSubsystem;
+private final Kicker kickerSubsystem;
 private final boolean startShooter;
 
-//private DigitalInput linebreak;
 
-    public ShootingCmd(Shooter shooterSubsystem, Kicker kickerSubsystem,boolean startShooter){
+    public ShootingCmd(Shooter shooterSubsystem, Kicker kickerSubsystem, boolean startShooter){
         this.shooterSubsystem = shooterSubsystem;
         this.kickerSubsystem = kickerSubsystem;
         this.startShooter = startShooter;
-        //linebreak = new DigitalInput(8);
-        
 
-        addRequirements(shooterSubsystem);
+        addRequirements(shooterSubsystem, kickerSubsystem);
     }
-  boolean warming;
+  //boolean warming;
 
   @Override
   public void initialize() {
-    
-    if (shooterSubsystem.sendSetpoint == ShooterSetpoint.zero && !warming){
-      shooterSubsystem.startShooter(ShooterSetpoint.speakerSetpoint);
-      warming = true;
-    } else if (shooterSubsystem.sendSetpoint == ShooterSetpoint.speakerSetpoint){
-      kickerSubsystem.startKicker(true);
+
+    if(shooterSubsystem.switched && IntakeCmd.isLineBroken()){
+      shooterSubsystem.setWarming();
+      shooterSubsystem.switched = false;
+    } else if (IntakeCmd.isLineBroken()){
+      shooterSubsystem.setShoot();
+      shooterSubsystem.switched = true;
+    } else {
+      cancel();
     }
+    
   }
 
   @Override
   public void execute() {
-    // if (!linebreak.get()){
-    //   shooterSubsystem.startShooter(false);
-    // }
+    if(!IntakeCmd.isLineBroken()){
+      shooterSubsystem.setIdle();
+    }
   }
 
   @Override
   public void end(boolean interrupted) {
-    shooterSubsystem.startShooter(ShooterSetpoint.zero);
+    System.out.println("finished");
   }
 
   @Override
